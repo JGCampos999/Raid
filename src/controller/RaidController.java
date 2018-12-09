790150c3897e834ce2c8253b125f0ff161d2a9fe
Guilherme Campos <guicamposf16@gmail.com>
2018-12-09 13:04:11 -0200

    Raid 0 Simulador

--- src/controller/RaidController.java
+++ src/controller/RaidController.java
@@ -1,39 +1,130 @@
 package controller;
 
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.SwingUtilities;
 
-public class RaidController {
+public class RaidController extends JPanel {
+	static long tamanhoArquivo;
+	static int quantDiscos;
+	static long tamanho;
+	static boolean executou = false;
+	JProgressBar barra;
 
-	public void Simular(long tamanhoArquivo, long tamanho, int quantDiscos) {
-		long vetDisc[] = new long[quantDiscos];
-		System.out.print(
-				"Convertendo para bits, seu tamanho é de: " + tamanhoArquivo + " bits\r\nSendo assim, cada disco terá: "
-						+ (tamanhoArquivo / quantDiscos) + " bits\r\nE cada bloco de cada disco terá 32 bits\r\n"
-						+ "Dando o total de " + tamanho + " blocos\r\n\t\t");
-		for (int i = 0; i < quantDiscos; i++) {
-			System.out.print("Disk" + (i + 1) + "\t");
-			vetDisc[i] = (tamanhoArquivo / quantDiscos);
+	public RaidController() {
+		// initialize Progress Bar
+		barra = new JProgressBar();
+		barra.setMinimum(0);
+		add(barra);
+	}
+
+	public void Val() {
+		tamanhoArquivo = Integer
+				.parseInt(JOptionPane.showInputDialog("Insira o tamanho do arquivo sem a medida de dados:"));
+		while (tamanhoArquivo <= 0) {
+			JOptionPane.showMessageDialog(null, "Valor inválido, tente novamente");
+			tamanhoArquivo = Integer
+					.parseInt(JOptionPane.showInputDialog("Insira o tamanho do arquivo sem a medida de dados:"));
 		}
-		System.out.print("\r\n");
+		Object[] opcoes = { "Selecione uma opção", tamanhoArquivo + " Bytes", tamanhoArquivo + " Kilobytes",
+				tamanhoArquivo + " Megabytes", tamanhoArquivo + " Gigabytes" };
+		Object res = JOptionPane.showInputDialog(null, "Qual das opções abaixo representa a medida de dados?",
+				"Seleção de itens", JOptionPane.PLAIN_MESSAGE, null, opcoes, "");
 
-		for (int i = 0; i < tamanho; i++) {
-			if ((i + 1) >= 100) {
-				System.out.print("Bloco" + (i + 1) + "\t");
-			} else {
-				System.out.print("Bloco" + (i + 1) + "\t\t");
-			}
-			
-			for (int j = 0; j < quantDiscos; j++) {
-				if (vetDisc[j] < 32) {
-					System.out.print(vetDisc[j] + "\t");
-					vetDisc[j] = 0;
-				} else {
-					vetDisc[j] -= 32;
-					System.out.print("32\t");
-				}
+		while (res == opcoes[0]) {
+			JOptionPane.showMessageDialog(null, "Opção inválida");
+			res = JOptionPane.showInputDialog(null, "Qual das opções abaixo representa a medida de dados?",
+					"Seleção de itens", JOptionPane.PLAIN_MESSAGE, null, opcoes, "");
+		}
+
+		if (res == opcoes[1]) {
+			tamanhoArquivo *= 8;
+		} else if (res == opcoes[2]) {
+			tamanhoArquivo *= (8 * Math.pow(10, 3));
+		} else if (res == opcoes[3]) {
+			tamanhoArquivo *= (8 * Math.pow(10, 6));
+		} else if (res == opcoes[4]) {
+			tamanhoArquivo *= (8 * Math.pow(10, 9));
+		}
+
+		quantDiscos = Integer.parseInt(JOptionPane.showInputDialog("Insira a quantidade de discos:"));
+		while (quantDiscos <= 0) {
+			JOptionPane.showMessageDialog(null, "Valor inválido, tente novamente");
+			quantDiscos = Integer.parseInt(JOptionPane.showInputDialog("Insira a quantidade de discos:"));
+		}
+		if (tamanhoArquivo % 32 != 0) {
+			tamanho = ((int) Math.round((tamanho / 32) + 1));
+		} else {
+			tamanho = (tamanhoArquivo / 32);
+		}
+		if (tamanho % quantDiscos != 0) {
+			tamanho = ((int) Math.round((tamanho / quantDiscos) + 1));
+		} else {
+			tamanho = (tamanho / quantDiscos);
+		}
+		executou = true;
+		barra.setMaximum((int) tamanho);
+		JOptionPane.showMessageDialog(null, "Valores carregados com sucesso.");
+	}
+
+	public void Simular() {
+		if (executou) {
+			final RaidController prct = new RaidController();
+			JFrame frame = new JFrame("Progress Bar Example");
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setContentPane(prct);
+			frame.pack();
+			frame.setVisible(true);
+			long vetDisc[] = new long[quantDiscos];
+			JOptionPane.showMessageDialog(null, "Convertendo para bits, seu tamanho é de: " + tamanhoArquivo
+					+ " bits.\r\nSendo assim, cada disco terá: " + (tamanhoArquivo / quantDiscos)
+					+ " bits.\r\nAlém disso, cada bloco de cada disco terá no máximo 32 bits.\r\n" + "Dando o total de "
+					+ tamanho + " bloco(s).\r");
+			System.out.print("\t\t");
+			for (int i = 0; i < quantDiscos; i++) {
+				System.out.print("Disk" + (i + 1) + "\t");
+				vetDisc[i] = (tamanhoArquivo / quantDiscos);
 			}
 			System.out.print("\r\n");
+
+			for (int i = 1; i <= tamanho; i++) {
+				final int porcentagem = ((int) Math.round((i * 100) / tamanho));
+				try {
+					SwingUtilities.invokeLater(new Runnable() {
+						public void run() {
+							prct.updateBar(porcentagem);
+						}
+					});
+					java.lang.Thread.sleep(100);
+				} catch (InterruptedException e) {
+					;
+				}
+				if ((i + 1) >= 100) {
+					System.out.print("Bloco" + i + "\t");
+				} else {
+					System.out.print("Bloco" + i + "\t\t");
+				}
+
+				for (int j = 0; j < quantDiscos; j++) {
+					if (vetDisc[j] < 32) {
+						System.out.print(vetDisc[j] + "\t");
+						vetDisc[j] = 0;
+					} else {
+						vetDisc[j] -= 32;
+						System.out.print("32\t");
+					}
+				}
+				System.out.print("\r\n");
+			}
+			JOptionPane.showMessageDialog(null, "Simulação Concluída");
+		} else {
+			JOptionPane.showMessageDialog(null, "É necessário inserir valores ao menos uma vez.");
 		}
-		JOptionPane.showMessageDialog(null, "Simulação Concluída");
+	}
+
+	public void updateBar(int newValue) {
+		barra.setValue(newValue);
 	}
 }
--- src/view/RaidViewer.java
+++ src/view/RaidViewer.java
@@ -1,15 +1,11 @@
 package view;
 
-import java.io.IOException;
 import javax.swing.JOptionPane;
 import controller.RaidController;
 
 public class RaidViewer {
-	static long tamanhoArquivo;
-	static int quantDiscos;
-	static long tamanho;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		RaidController control = new RaidController();
 		int Menu = 0;
 
@@ -18,14 +14,10 @@ public class RaidViewer {
 					"1 - Inserir Valores\n2 - Simular Raid 0 com valores inseridos e exibir resultados\n9 - Fim"));
 			switch (Menu) {
 			case (1):
-				Val();
+				control.Val();
 				break;
 			case (2):
-				if (tamanhoArquivo == 0) {
-					JOptionPane.showMessageDialog(null, "Insira os valores primeiro");
-				} else {
-					control.Simular(tamanhoArquivo, tamanho, quantDiscos);
-				}
+				control.Simular();
 				break;
 			case (9):
 				JOptionPane.showMessageDialog(null, "Encerrando...");
@@ -35,49 +27,6 @@ public class RaidViewer {
 				JOptionPane.showMessageDialog(null, "Opção inválida, tente novamente");
 			}
 		} while (Menu != 9);
-	}
-
-	public static void Val() {
-		tamanhoArquivo = Integer
-				.parseInt(JOptionPane.showInputDialog("Insira o tamanho do arquivo sem a medida de dados:"));
-		while (tamanhoArquivo <= 0) {
-			JOptionPane.showMessageDialog(null, "Valor inválido, tente novamente");
-			tamanhoArquivo = Integer
-					.parseInt(JOptionPane.showInputDialog("Insira o tamanho do arquivo sem a medida de dados:"));
-		}
-		Object[] opcoes = { "Selecione uma opção", tamanhoArquivo + " Bytes", tamanhoArquivo + " Kilobytes",
-				tamanhoArquivo + " Megabytes", tamanhoArquivo + " Gigabytes" };
-		Object res = JOptionPane.showInputDialog(null, "Qual das opções abaixo representa a medida de dados?",
-				"Seleção de itens", JOptionPane.PLAIN_MESSAGE, null, opcoes, "");
-
-		while (res == opcoes[0]) {
-			JOptionPane.showMessageDialog(null, "Opção inválida");
-			res = JOptionPane.showInputDialog(null, "Qual das opções abaixo representa a medida de dados?",
-					"Seleção de itens", JOptionPane.PLAIN_MESSAGE, null, opcoes, "");
-		}
-
-		if (res == opcoes[1]) {
-			tamanhoArquivo *= 8;
-		} else if (res == opcoes[2]) {
-			tamanhoArquivo *= (8 * Math.pow(10, 3));
-		} else if (res == opcoes[3]) {
-			tamanhoArquivo *= (8 * Math.pow(10, 6));
-		} else if (res == opcoes[4]) {
-			tamanhoArquivo *= (8 * Math.pow(10, 9));
-		}
-
-		quantDiscos = Integer.parseInt(JOptionPane.showInputDialog("Insira a quantidade de discos:"));
-		while (quantDiscos <= 0) {
-			JOptionPane.showMessageDialog(null, "Valor inválido, tente novamente");
-			quantDiscos = Integer.parseInt(JOptionPane.showInputDialog("Insira a quantidade de discos:"));
-		}
-		
-		tamanho = (tamanhoArquivo / 32);
-		if (tamanho % quantDiscos != 0) {
-			tamanho = ((int) Math.round((tamanho / quantDiscos) + 1));
-		} else {
-			tamanho = (tamanho / quantDiscos);
-		}
 	}
 
 }
